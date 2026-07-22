881e9d74ef57fdf07f7e1facc510a9795ec40fb3
MrNTDQ7 <ngotienduc0976423144@gmail.com>
2026-07-22 23:57:53 +0700

    Update index.ts

--- packages/ui/src/i18n/locales/vi-VN/index.ts
+++ packages/ui/src/i18n/locales/vi-VN/index.ts
@@ -2,7 +2,7 @@ import enUS from '../en-US'
 import core from './core'
 // Khi dịch thêm file nào thì bỏ comment dòng import + dòng spread tương ứng bên dưới
 // import favorites from './favorites'
-// import prompt from './prompt'
+import prompt from './prompt'
 // import models from './models'
 // import templates from './templates'
 // import testing from './testing'
